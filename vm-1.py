f7c1aae47883158e3989ed5dbd0b21aa969825c7
V Venkatesh Kumar <57843495+VVK-3@users.noreply.github.com>
2021-01-30 23:27:12 +0530

    Add files via upload

---
+++ vm-1.py
@@ -0,0 +1,34 @@
+#modified by VVK-3 31-01-2021
+#acting as virtual meter
+
+import requests
+import json
+import random
+import time
+
+meter_address = "vm-1"
+
+i = 1
+
+while i <= 20:
+    data = {}
+    data['meter_address'] = meter_address
+    data['meter_usage'] = random.randint(5,15)
+    data['timestamp'] = time.time()
+
+    print(data)
+
+    time.sleep(2)
+
+    t = requests.post("http://127.0.0.1:5000/transactions/new", json=data)
+
+    print(t.text)
+
+    if i%4==0:
+        # Mine the pushed transactions for a single block
+        m = requests.get("http://127.0.0.1:5000/mine")
+        j_m = json.loads(m.text)
+
+    i += 1
+
+
